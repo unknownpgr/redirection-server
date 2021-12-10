cc0bcd816558b920ab4651d932e13b824a2a836b
unknownpgr <unknownpgr@gmail.com>
2021-12-10 14:08:38 +0000

    Implement back / frontend

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const { createClient } = require('redis');
+const Koa = require('koa');
+const Router = require('koa-router');
+const body = require('koa-bodyparser');
+const serve = require('koa-static');
+const path = require('path');
+
+async function main() {
+  const client = createClient({
+    url: 'redis://redis:6379'
+  });
+  client.on('error', (err) => console.log('Redis Client Error', err));
+  await client.connect();
+
+  const app = new Koa();
+  const router = new Router();
+
+  const prefiex = prefix => value => `${prefix}${value}`;
+  const h = prefiex('h'); // Prefix for hash
+  const u = prefiex('u'); // Prefix for URL
+  const k = prefiex('k'); // Prefix for key
+
+  router.get('/:hash', async ctx => {
+    const { hash } = ctx.params;
+    if (!hash) {
+      return;
+    }
+    const url = await client.get(h(hash));
+    if (url) ctx.redirect(url);
+    else ctx.response.status = 404;
+  });
+
+  router.post('/', body(), async ctx => {
+    const { url } = ctx.request.body;
+    if (!url) {
+      ctx.response.status = 400;
+      return;
+    }
+
+    const hash = await client.get(u(url));
+    if (hash) {
+      // Cache hit
+      ctx.body = hash;
+    } else {
+      // Cache miss
+      const newHash = `${await client.incr(k('hash'))}`;
+      // Save both key-value and value-key
+      await client.set(h(newHash), url);
+      await client.set(u(url), newHash);
+      ctx.body = newHash;
+    }
+  });
+
+  app.use(router.routes());
+  app.use(serve(path.join(__dirname, '/static')));
+  app.listen(80);
+}
+
+main();
